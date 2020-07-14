6c783c043567fab1988b051ff282fe54fcfba73f
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-07-14 19:36:39 -0400

    Se creo la carpeta BLL y se añdio UsuariosBLL con sus metodos ademas del diseño del login

---
+++ ProyectoFinalAplicada1/DAL/Contexto.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAplicada1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalAplicada1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging().UseSqlite(@"Data Source= DATA\Usuario.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+           /* modelBuilder.Entity<Usuarios>().HasData(new Usuarios
+            {
+                UsuarioId = 1,
+                Nombres = "Eladio.",
+                Apellidos = "Jiménez L.",
+                NombreUsuario = "user01",
+                Contrasena = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
+            });*/
+        }
+
+    }
+}
