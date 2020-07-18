bc900549fa660304a026fd8749f26c99f21dfb74
Mario <Mario@DESKTOP-JLRVOOA>
2020-07-18 19:24:34 -0400

    longin listo

--- ProyectoFinalAplicada1/Properties/Resources.Designer.cs
+++ ProyectoFinalAplicada1/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace ProyectoFinalAplicada1.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] entrar {
+            get {
+                object obj = ResourceManager.GetObject("entrar", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
