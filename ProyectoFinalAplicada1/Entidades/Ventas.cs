351dd7c252ff194cbd234eeabd99d0451749ff09
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-07-22 20:03:22 -0400

    Se añadio la clase Ventas y ventasDetalle

--- ProyectoFinalAplicada1/Entidades/Ventas.cs
+++ ProyectoFinalAplicada1/Entidades/Ventas.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ProyectoFinalAplicada1.Entidades
 {
-    class Ventas
+    public class Ventas
     {
+
+        [Key]
+        public int VentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public decimal Monto { get; set; }
+        public int UsuarioId { get; set; }
+
+
+        [ForeignKey("VentaId")]
+        public virtual List<VentasDetalles> VentaDetalle { get; set; }
+
+        public Ventas()
+        {
+            VentaId = 0;
+            
+            Fecha = DateTime.Now;
+            ClienteId = 0;
+            Monto = 0;
+            UsuarioId = 0;
+
+            VentaDetalle = new List<VentasDetalles>();
+        }
+
     }
 }
---
+++ ProyectoFinalAplicada1/Entidades/VentasDetalles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProyectoFinalAplicada1.Entidades
+{
+    public class VentasDetalles
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        //public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public decimal ITBIS { get; set; }
+
+        public VentasDetalles()
+        {
+            Id = 0;
+            VentaId = 0;
+            ProductoId = 0;
+            Cantidad = 0;
+            //Descripcion = string.Empty;
+            Precio = 0;
+            ITBIS = 0;
+        }
+
+        public VentasDetalles(int ventaid, int productoid, int cantidad, string descripcion, decimal precio)
+        {
+            Id = 0;
+            VentaId = ventaid;
+            ProductoId = productoid;
+            Cantidad = cantidad;
+            //Descripcion = descripcion;
+            Precio = precio;
+            ITBIS = cantidad * precio;
+
+        }
+
+    }
+}
