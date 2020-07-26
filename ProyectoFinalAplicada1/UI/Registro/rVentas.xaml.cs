2057a4a4f950353af4638d520b3a8894e6ba4712
Mario <Mario@DESKTOP-JLRVOOA>
2020-07-26 16:19:22 -0400

    detalle de buscar venta listo

--- ProyectoFinalAplicada1/UI/Registro/rVentas.xaml.cs
+++ ProyectoFinalAplicada1/UI/Registro/rVentas.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProyectoFinalAplicada1.BLL;
+using ProyectoFinalAplicada1.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,14 +19,59 @@ namespace ProyectoFinalAplicada1.UI.Registro
     /// </summary>
     public partial class rVentas : Window
     {
+        Ventas venta = new Ventas();
         public rVentas()
         {
             InitializeComponent();
+            this.DataContext = venta;
+            VentaIdTextBox.Text = "0";
+        }
+
+        private void ProductoIdTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ProductoIdTextBox.Text.Length == 0)
+                return;
+
+            Productos producto = ProductosBLL.Buscar(Convert.ToInt32(ProductoIdTextBox.Text));
+            if (producto == null)
+            {
+                MessageBox.Show("producto no existe");
+                DescripcionTextBox.Text = "";
+            }
+            else
+            {
+                DescripcionTextBox.Text = producto.Descripcion;
+            }            
         }
 
         private void BucarButton_Click(object sender, RoutedEventArgs e)
         {
+            var venta = VentasBLL.Buscar(int.Parse(VentaIdTextBox.Text));
 
+            if (venta != null)
+            {
+                this.venta = venta;
+            }
+            else
+            {
+                this.venta = new Entidades.Ventas();
+                MessageBox.Show("El Usuario no existe", "Fallo",
+                     MessageBoxButton.OK, MessageBoxImage.Information);
+                Limpiar();
+            }
+
+            //Limpiar();
+            this.DataContext = this.venta;
         }
+
+        private void Limpiar()
+        {
+            VentaIdTextBox.Text = "0";
+           // NombresTextBox.Text = string.Empty;
+           // ApellidosTextBox.Text = string.Empty;
+           // UsuarioIdTextBox.Text = 0;
+        }
+
+
     }
 }
