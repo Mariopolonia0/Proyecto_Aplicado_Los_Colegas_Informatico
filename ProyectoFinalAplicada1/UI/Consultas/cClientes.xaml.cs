7de080fb577e90fabc993b8239eab7255da5422a
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-08-03 11:56:01 -0400

    Consultas casi listas.

--- ProyectoFinalAplicada1/UI/Consultas/cClientes.xaml.cs
+++ ProyectoFinalAplicada1/UI/Consultas/cClientes.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProyectoFinalAplicada1.BLL;
+using ProyectoFinalAplicada1.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -20,6 +22,48 @@ namespace ProyectoFinalAplicada1.UI.Consultas
         public cClientes()
         {
             InitializeComponent();
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Clientes>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //ClienteId
+                        listado = ClientesBLL.GetList(e => e.ClienteId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1: //Nombres                       
+                        listado = ClientesBLL.GetList(e => e.Nombres.Contains(CriterioTextBox.Text, StringComparison.OrdinalIgnoreCase));
+                        break;
+
+                    case 2: //Apellidos                       
+                        listado = ClientesBLL.GetList(e => e.Apellidos.Contains(CriterioTextBox.Text, StringComparison.OrdinalIgnoreCase));
+                        break;
+
+                    case 3: //Telefono                       
+                        listado = ClientesBLL.GetList(e => e.Telefono.Contains(CriterioTextBox.Text, StringComparison.OrdinalIgnoreCase));
+                        break;
+
+                    case 4: //Cedula                       
+                        listado = ClientesBLL.GetList(e => e.Cedula.Contains(CriterioTextBox.Text, StringComparison.OrdinalIgnoreCase));
+                        break;
+
+                    case 5: //Direccion                       
+                        listado = ClientesBLL.GetList(e => e.Direccion.Contains(CriterioTextBox.Text, StringComparison.OrdinalIgnoreCase));
+                        break;
+                }
+            }
+            else
+            {
+                listado = ClientesBLL.GetList(c => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
